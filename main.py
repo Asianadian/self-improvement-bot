be9b5f0a02d010548acfe76f5ff281ad5f810775
Asianadian <danielchen.mc@gmail.com>
2024-05-12 15:24:38 -0400

    Set up logging library

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import os
 import discord
 import dotenv
+import logging as log
+
+from logging.handlers import TimedRotatingFileHandler
+from logging import Formatter
+
+defaultFormatter = Formatter(
+  fmt="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+  datefmt="%d/%b/%Y %H:%M:%S"
+)
+
+defaultHandler = TimedRotatingFileHandler(
+  filename='discord.log', 
+  when='D', 
+  interval=1, 
+  backupCount=1, 
+  encoding='utf-8', 
+  delay=False,
+)
+
+defaultHandler.setFormatter(defaultFormatter)
+
+log.basicConfig(level=log.DEBUG)
+log.getLogger().handlers.clear()
+log.getLogger().addHandler(defaultHandler)
 
 dotenv.load_dotenv()
-
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents=discord.Intents.default())
+class CustomClient(discord.Client):
+  def __init__(self):
+    log.debug('Custom client initiated.')
+    super().__init__(intents=discord.Intents.default())
 
-@client.event
-async def on_ready():
-  print(f'{client.user} has connected to Discord!')
+  async def on_ready(self):
+    log.debug(f'{client.user} has connected to Discord!')
+    log.debug(f'{client.guilds[0]} id: {client.guilds[0].id}')
 
-client.run(DISCORD_TOKEN)
+client = CustomClient()
+client.run(DISCORD_TOKEN,log_handler=None)
 
